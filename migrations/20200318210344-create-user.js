954b707eab6e3cb8d5569d6aa4a003cad09603bc
LaurynasKraujalis <laurynas_k@hotmail.com>
2020-05-21 11:55:16 +0200

    changed user model/migration to include more columns

--- migrations/20200318210344-create-user.js
+++ migrations/20200318210344-create-user.js
@@ -6,32 +6,47 @@ module.exports = {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       name: {
         type: Sequelize.STRING,
-        allowNull: false
+        allowNull: false,
       },
       email: {
         type: Sequelize.STRING,
         unique: true,
-        allowNull: false
+        allowNull: false,
       },
       password: {
         type: Sequelize.STRING,
-        allowNull: false
+        allowNull: false,
+      },
+      image: {
+        type: DataTypes.TEXT,
+
+        defaultValue: null,
+      },
+      motto: {
+        type: DataTypes.STRING,
+
+        defaultValue: null,
+      },
+      story: {
+        type: DataTypes.TEXT,
+
+        defaultValue: null,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   down: (queryInterface, Sequelize) => {
     return queryInterface.dropTable("users");
-  }
+  },
 };

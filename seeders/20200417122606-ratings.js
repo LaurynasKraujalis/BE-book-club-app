b3232059ecc02316d8bd3562bbef81e5c7fdac0b
LaurynasKraujalis <laurynas_k@hotmail.com>
2020-05-28 10:26:18 +0200

    commented out all the seeders

--- seeders/20200417122606-ratings.js
+++ seeders/20200417122606-ratings.js
@@ -1,58 +1,58 @@
-"use strict";
+// "use strict";
 
-module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    return await queryInterface.bulkInsert(
-      "ratings",
-      [
-        {
-          rating: 3,
-          userId: 2,
-          bookId: 1,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          rating: 3,
-          userId: 2,
-          bookId: 1,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          rating: 3,
-          userId: 2,
-          bookId: 1,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          rating: 5,
-          userId: 2,
-          bookId: 2,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          rating: 2,
-          userId: 1,
-          bookId: 1,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          rating: 5,
-          userId: 1,
-          bookId: 2,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-      ],
-      {}
-    );
-  },
+// module.exports = {
+//   up: async (queryInterface, Sequelize) => {
+//     return await queryInterface.bulkInsert(
+//       "ratings",
+//       [
+//         {
+//           rating: 3,
+//           userId: 2,
+//           bookId: 1,
+//           createdAt: new Date(),
+//           updatedAt: new Date(),
+//         },
+//         {
+//           rating: 3,
+//           userId: 2,
+//           bookId: 1,
+//           createdAt: new Date(),
+//           updatedAt: new Date(),
+//         },
+//         {
+//           rating: 3,
+//           userId: 2,
+//           bookId: 1,
+//           createdAt: new Date(),
+//           updatedAt: new Date(),
+//         },
+//         {
+//           rating: 5,
+//           userId: 2,
+//           bookId: 2,
+//           createdAt: new Date(),
+//           updatedAt: new Date(),
+//         },
+//         {
+//           rating: 2,
+//           userId: 1,
+//           bookId: 1,
+//           createdAt: new Date(),
+//           updatedAt: new Date(),
+//         },
+//         {
+//           rating: 5,
+//           userId: 1,
+//           bookId: 2,
+//           createdAt: new Date(),
+//           updatedAt: new Date(),
+//         },
+//       ],
+//       {}
+//     );
+//   },
 
-  down: async (queryInterface, Sequelize) => {
-    return await queryInterface.bulkDelete("ratings", null, {});
-  },
-};
+//   down: async (queryInterface, Sequelize) => {
+//     return await queryInterface.bulkDelete("ratings", null, {});
+//   },
+// };
--- seeders/20200422133107-reactions.js
+++ seeders/20200422133107-reactions.js
@@ -1,37 +1,37 @@
-"use strict";
+// "use strict";
 
-module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    return await queryInterface.bulkInsert(
-      "reactions",
-      [
-        {
-          reaction: "This is hilarious!",
-          commentId: 1,
-          userId: 2,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          reaction: "Thats rude to say.",
-          commentId: 1,
-          userId: 1,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          reaction: "I respectfully disagree with your opinion.",
-          commentId: 2,
-          userId: 1,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-      ],
-      {}
-    );
-  },
+// module.exports = {
+//   up: async (queryInterface, Sequelize) => {
+//     return await queryInterface.bulkInsert(
+//       "reactions",
+//       [
+//         {
+//           reaction: "This is hilarious!",
+//           commentId: 1,
+//           userId: 2,
+//           createdAt: new Date(),
+//           updatedAt: new Date(),
+//         },
+//         {
+//           reaction: "Thats rude to say.",
+//           commentId: 1,
+//           userId: 1,
+//           createdAt: new Date(),
+//           updatedAt: new Date(),
+//         },
+//         {
+//           reaction: "I respectfully disagree with your opinion.",
+//           commentId: 2,
+//           userId: 1,
+//           createdAt: new Date(),
+//           updatedAt: new Date(),
+//         },
+//       ],
+//       {}
+//     );
+//   },
 
-  down: async (queryInterface, Sequelize) => {
-    return await queryInterface.bulkDelete("reactions", null, {});
-  },
-};
+//   down: async (queryInterface, Sequelize) => {
+//     return await queryInterface.bulkDelete("reactions", null, {});
+//   },
+// };
